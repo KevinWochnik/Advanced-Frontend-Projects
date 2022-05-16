c9303173724a626bb3271da7c073516231e61b85
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-05-16 21:24:00 +0200

    add swiper

--- learning/section-9 project-scroller/src/main.js
+++ learning/section-9 project-scroller/src/main.js
@@ -2,5 +2,6 @@ import { Scroller } from "./Scroller.js";
 document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller(".main");
   document.addEventListener("wheel", scroller.listenScroll);
-
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });

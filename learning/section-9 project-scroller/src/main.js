0c88cd7d38b5a6a73089c3530412b609e682e502
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-05-16 14:24:16 +0200

    commit changes before refractor

--- learning/section-9 project-scroller/src/main.js
+++ learning/section-9 project-scroller/src/main.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("works");
-
   const rootElement = document.querySelector(".main");
   const sections = [...document.querySelectorAll(".main__section")];
+  let currentSectionIndex = 0;
+  let isThrottled = false;
 
   document.addEventListener("wheel", (e) => {
-    console.log("scroll");
+    if (isThrottled) return;
+    isThrottled = true;
+    setTimeout(() => {
+      isThrottled = false;
+    }, 1000);
+    const scrollDirection = e.wheelDelta < 0 ? 1 : -1;
+    if (scrollDirection === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (scrollDirection === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+    currentSectionIndex = currentSectionIndex + scrollDirection;
+    console.log(currentSectionIndex);
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "center",
+    });
   });
 });
